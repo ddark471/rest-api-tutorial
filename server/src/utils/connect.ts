a3a69472fb056474ff5a11bf4cab42cf84a91d84
dark <ddarkk471@gmail.com>
2024-11-21 01:31:59 +0500

    fix(utils/connect): fixed db url

--- server/src/utils/connect.ts
+++ server/src/utils/connect.ts
@@ -3,7 +3,7 @@ import c from "config";
 import { createRequire } from "node:module";
 
 async function connect (){
-    const dbUri = "mongodb+srv://ddarkk471:j0Lj24tVY6VRw5xQ@cluster0.7u6hh.mongodb.net"
+    const dbUri = "mongodb+srv://ddarkk471:j0Lj24tVY6VRw5xQ@cluster0.7u6hh.mongodb.net/qahva"
     const require = createRequire(import.meta.url);
     const mongoose = require("mongoose")
     try{
