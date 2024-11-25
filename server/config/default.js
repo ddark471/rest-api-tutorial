146cfa7ee8fa3a94392e683cacda757ee9095bce
dark <ddarkk471@gmail.com>
2024-11-25 15:04:16 +0500

    fix(config): moved to the root folder from src

---
+++ server/config/default.js
@@ -0,0 +1,9 @@
+const config = {
+    port: 8080,
+    dbUri: "mongodb+srv://ddarkk471:j0Lj24tVY6VRw5xQ@cluster0.7u6hh.mongodb.net",
+    saltWorkFactor: 10,
+    accessTokenTtl: "15m",
+    refreshTokenTtl: "1d"
+}
+
+export default config;
